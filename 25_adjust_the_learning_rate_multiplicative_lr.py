871c871303622c0e01850e55a6f030b0d77ccd8e
Andrey Pudov <mail@andreypudov.com>
2023-06-05 19:06:22 +0200

    Adjust the Learning Rate

---
+++ 25_adjust_the_learning_rate_multiplicative_lr.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.optim.lr_scheduler as lr_scheduler
+
+lr = 0.1
+model = nn.Linear(10, 1)
+
+optimizer = torch.optim.SGD(model.parameters(), lr = lr)
+
+lambda1 = lambda epoch: 0.95
+scheduler = lr_scheduler.MultiplicativeLR(optimizer, lr_lambda = lambda1)
+
+print(optimizer.state_dict())
+for epoch in range(5):
+    # loss.backward()
+    optimizer.step()
+
+    # validate(...)
+    scheduler.step()
+
+    print(optimizer.state_dict()['param_groups'][0]['lr'])
